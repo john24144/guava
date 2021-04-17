751f13c2c6f612148e1f60c9dce7790994730c20
john24144 <kjohnson@manifestcorp.com>
2021-04-17 12:52:45 -0400

    issue #5296: RateLimiter is no accuracy

--- guava-tests/test/com/google/common/util/concurrent/SmoothRateLimiterTest.java
+++ guava-tests/test/com/google/common/util/concurrent/SmoothRateLimiterTest.java
@@ -3,6 +3,8 @@ package com.google.common.util.concurrent;
 import static org.junit.Assert.assertEquals;
 
 import com.google.common.util.concurrent.RateLimiterTest.FakeStopwatch;
+import com.sun.org.slf4j.internal.Logger;
+import com.sun.org.slf4j.internal.LoggerFactory;
 import org.junit.Test;
 
 public class SmoothRateLimiterTest {
@@ -11,9 +13,26 @@ public class SmoothRateLimiterTest {
 
   @Test
   public void reserveEarliestAvailable() {
-    RateLimiter limiter = SmoothRateLimiter.create(5.0, stopwatch);
+    long limit = 80000;
+    RateLimiter limiter = SmoothRateLimiter.create(limit, stopwatch);
     long expected = limiter.reserveEarliestAvailable(0, 0);
     assertEquals(expected, 0);
+
+
+    /*
+    long aquired = 0L;
+    while (true) {
+      if (limiter.tryAcquire()) {
+        ++aquired;
+      }
+      if (aquired == limit) {
+        logger.trace("Acquire" + aquired + ", cost: " + stopwatch.readMicros() + "ms");
+        aquired = 0;
+        stopwatch.readEventsAndClear();
+      }
+    }
+
+     */
   }
 
 }
